1a2b95eeda7714c465f88e83c160a55cf4818cbe
Emmanuel Diaz <emdiaz@e1z3r3p21.42.us.org>
2019-06-17 01:19:41 -0700

    A simpler version

--- get_next_line.c
+++ get_next_line.c
@@ -1,9 +1,25 @@
 #include "get_next_line.h"
 #include <stdio.h>
+#include <fcntl.h>
 
 int     get_next_line(const int fd, char **line)
-{		
-		*line = ft_strnew(BUFF_SIZE);
-		read(fd, *line, BUFF_SIZE);
+{
+		int	ret_r;
+		static char *str[MAX_FD];
+		char *tmp;
+		char buf[BUFF_SIZE + 1];
+
+		if (fd == -1 || !line || !(str[fd] = !str[fd] ? ft_strnew(0) : str[fd]))
+			return (-1);
+		while ((ret_r = read(fd,buf,BUFF_SIZE)) > 0 && !(ft_strchr(str[fd], '\n')))
+		{
+			tmp = str[fd];
+			buf[ret_r] = '\0';
+			str[fd] = ft_strjoin(tmp,buf);
+			free(tmp);
+		}	
+		ret_r == -1 ? return (-1) : 0;
+		*line = (ft_strchr(str[fd], '\n') ? ft_strsub(str[fd], 0, ft_strchr(str[fd], '\n') - str[fd]) : ft_strdup(s[fd]);	
+		
 		return (0);
 }
--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
-
-int main()
+int main(int argc, char **argv)
 {
 	char **line;
 	int fd;
 	int	numero_lineas;
 	char jol;
 	
-
+	(void)argc;
 	numero_lineas = 0;
-	fd = open("txt.txt", O_RDONLY, 0);
+	fd = open(argv[1], O_RDONLY);
 
 	while (read(fd, &jol,1) > 0)
 		if (jol == '\n')
@@ -19,12 +18,11 @@ int main()
 	line = ft_strnew2ptr(numero_lineas); // Fx() new for the 2ptr arrary
 	close(fd);	
 
-	fd = open("txt.txt",O_RDONLY, 0);
+	fd = open(argv[1],O_RDONLY);
 	
-	get_next_line(fd, line);
-		
 	printf("#%d of lines found in document\n", numero_lineas);
 	printf("FD assigned by the system is; %d\n", fd);
     printf("[%s]\n", *line);
+	get_next_line(fd, line);
 	return 0;
 }
