251a2dfef4baccb2bb246e88024679747200f085
Emmanuel Diaz <emdiaz@e1z3r1p14.42.us.org>
2019-06-27 21:05:06 -0700

    read line

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,116 @@
 
 int		get_next_line(const int fd, char **line)
 {
+	int r;
+	char *ss[MAX_FD];
+	char b[BUFF_SIZE + 1];
+	char *t;
+	char *box;	
+	
+	if  (!ss[fd])
+		ss[fd] = ft_strnew(1);
+	while ((r = read(fd,b,BUFF_SIZE)) > 0)
+	{
+		b[r] = '\0';
+		box = ft_strjoin(ss[fd],b);
+		ft_strdel(&ss[fd]);
+		ss[fd] = box;
+		if ((t = ft_strchr(ss[fd],'\n')))
+		{
+			break;
+		}
+	}
+	if (r == -1)
+	{
+		return (-1);
+	}
+	if (r ==  0 && ss[fd] == NULL)
+	{
+		ft_strdel(&ss[fd]);
+		return (0);
+	}
+	else if (r == 0 && ss[fd] && !(t = ft_strchr(ss[fd],'\n')))
+	{
+		*line = ft_strdup(ss[fd]);
+		ft_strdel(&ss[fd]);
+	}
+	else
+	{
+		*t = '\0';
+		*line = ft_strdup(ss[fd]);
+		t++;
+		box = ft_strdup(t);
+		ft_strdel(&ss[fd]);
+		ss[fd] = box;
+	}
+	return (1);
+}
+/*
+	int			r;
+	char		*b;
+	char		*t;
+	char static	*ss[MAX_FD];
+
+	b = ft_strnew(100);
+	r = read(fd,b,BUFF_SIZE);
+	while (r > 0)
+	{	
+		t = ft_strdup(b);
+		printf("%s",t);
+			if ((t = ft_strchr(b,'\n')))
+			{
+				ss[fd] = ft_strdup(t);
+				printf("%p\n", &(*t));
+				printf("%s\n", ss[fd]);
+				*t = '\0';
+			}	
+			break;
+	}
+	(void)line;
+	
+		
+	
 	int				r;
-	char			*t2;
+	char			*n;
 	char			*b;
 	char			*t;
-	static char		*ss[MAX_FD];	
+	static char		*ss[MAX_FD];
 	
+	r = read(fd,b,BUFF_SIZE);
 
-	while ()
+	if (r < 0 || !line || fd < 0)
+		return (-1);
+	while (r > 0)
+	{
+		b = ft_strnew(1);
+		b[r] = '\0';
+		if (ss[fd] == NULL)
+			t = ft_strjoin(ss[fd],b);
+		free(ss[fd]);
+		ss[fd] = t;
+		n = strchr(b, '\n');
+		if (n)
+			break;
+	}
+
+	 	
+
+	(void)line;	
+	b = ft_strnew(5);
+	r = read(fd,b,5);
+	t = ft_strchr(b,'\n');
+
 	
-	/*
+	
+	if (r > 0)
+		{
+			while ()
+			{
+				printf("[b]%s\n", b);
+				printf("Pointer of b is at address: %p\n", &(*b));
+				b++;
+			}
+		}		
 	 *	//Creo b y utilizo strnew para alocar memoria en mi buf. 
 	 *	  b = ft_strnew(BUFF_SIZE);
 	 *  / b  = 9 + 1, b = 10
@@ -28,7 +128,6 @@ int		get_next_line(const int fd, char **line)
 	}	
 	
 	
-/*
 	b = ft_strnew(BUFF_SIZE);
 	r = read(fd,b,BUFF_SIZE);
 	printf("[Buf-Len] %d*\n",r);
@@ -48,6 +147,4 @@ int		get_next_line(const int fd, char **line)
 //	ss[fd] = ft_strdup(t);
 	printf("[SS deleted]:%s\n", ss[fd]);
 	*line = ft_strdup(t2);
-*/		
-	return 0;
-}
+*/
